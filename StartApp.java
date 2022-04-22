ba78fe86a2114b5b8fc62d981f7ce6cf46487859
Alyssonjff <104224074+Alyssonjff@users.noreply.github.com>
2022-04-22 18:00:38 -0300

    Add files via upload

---
+++ StartApp.java
@@ -0,0 +1,46 @@
+package br.edu.univas;
+
+public class StartApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+
+        int  idade = 18;
+        System.out.println(idade);
+        System.out.println("idade");
+
+        String nome = "Alysson Jose de Franca Ferreira";
+        System.out.println(nome);
+
+        float peso = 92.8f;
+        double peso2 = 92.9;
+        System.out.println(peso);
+
+        //variaveis primitivas//
+        short a = 10;
+        byte b = 11;
+        int c = 12;
+        long d = 13;
+        float e = 14.4f;
+        double f = 15.5;
+        char g = 'R';
+        boolean h = true;
+
+        String faculdade = "Univas";
+
+        String nomeADefinir = "";
+        String cidade = null;
+
+        /*
+        comentario
+        com
+        mais
+        de
+        uma
+        linha
+        */
+
+        System.out.println(nomeADefinir);
+        System.out.println(cidade);
+    }
+
+}
