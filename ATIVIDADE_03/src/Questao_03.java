d6d4af1c2978e82469eab62a6f8cc2a10569f002
Alysson <104224074+Alyssonjff@users.noreply.github.com>
2023-03-22 19:08:19 -0300

    Update Questao_03.java

--- ATIVIDADE_03/src/Questao_03.java
+++ ATIVIDADE_03/src/Questao_03.java
@@ -1,37 +1,24 @@
+/*
+ Escreva um programa que coloque todas as vogais de uma string em letra
+ maiúscula.
+ */
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map;
-public class Questao_03 {
-    public static void main(String[] args){
+public class Questao03 {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("digite algo");
-        String input = scan.nextLine();
-        String output = removeOddOccurrence(input);
-        System.out.println(output);
-    }
-
-    public static String removeOddOccurrence(String input) {
-        Map<Character, Integer> countMap = new HashMap<>();
-
-        // Conta as ocorrências de cada letra na string de entrada
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (countMap.containsKey(c)) {
-                countMap.put(c, countMap.get(c) + 1);
-            } else {
-                countMap.put(c, 1);
-            }
+        String readWord = "";
+        String vowelUppercase = "";
+        System.out.println("Por favor digite uma palavra: ");
+        readWord = scan.nextLine();
+        for(int i = 0; i < readWord.length(); i++){
+        char aux = readWord.charAt(i);
+        if(aux == 'a' || aux == 'e' || aux == 'i' || aux == 'o' || aux == 'u' ||
+                aux == 'A' || aux == 'E' || aux == 'I' || aux == 'O' || aux == 'U'){
+            vowelUppercase += Character.toUpperCase(aux);
+        }else {
+            vowelUppercase += aux;
         }
-
-        // Remove as letras que ocorrem um número ímpar de vezes
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (countMap.get(c) % 2 == 0) {
-                sb.append(c);
-            }
         }
-        return sb.toString();
+        System.out.println("A palavra digita com as vogais maiúscula: " + vowelUppercase);
     }
 }
-
