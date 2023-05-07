c512ba79af18f3a189325acb00702b427360362e
Alysson <104224074+Alyssonjff@users.noreply.github.com>
2023-05-06 22:28:02 -0300

    Update Livro.java

--- Starting_of_Oo/src/Exercicio07/Livro.java
+++ Starting_of_Oo/src/Exercicio07/Livro.java
@@ -1,4 +1,10 @@
 package Exercicio07;
 
 public class Livro {
+  public String titulo;
+  public String autor;
+  public int ano;
+  public void imprimir(){
+      System.out.println(this.titulo +" - " + this.autor + " - " + this.ano);
+  }
 }
